f3a6b0e38fd1ca25ab15c6115feb124345547c2d
mhmdwSamir <mhmdgezo1912@gmail.com>
2020-07-20 13:48:17 +0200

    adding hide or show password functionality in password feild

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  hide: true;
+  hide = true;
   formRegister = new FormGroup({
     name: new FormControl(null, [
       Validators.required,
